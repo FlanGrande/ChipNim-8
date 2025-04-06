94cb775a01989eb062290f6011017a7fd16396d1
FlanGrande <francisjgrande@gmail.com>
2025-04-06 20:02:42 +0100

    Set up chip8renderer properly

--- nim/src/classes/chip8renderer.nim
+++ nim/src/classes/chip8renderer.nim
@@ -1,16 +1,16 @@
 import gdext
 import chip8/chip8
 
-import gdext/classes/gdNode
-import gdext/classes/gdViewportTexture
+import gdext/classes/gdNode2D
 
-type Chip8Renderer* {.gdsync.} = ptr object of Node
+type Chip8Renderer* {.gdsync.} = ptr object of Node2D
+  number: int = 0
   chip8: Chip8
-  viewportTexture* {.gdexport.}: gdref ViewportTexture
 
 method ready(self: Chip8Renderer) {.gdsync.} =
   #self.chip8 = initChip8()
   print("ready")
+  # set_process_draw(true)
   # loadRom(self.chip8, "/roms/games/Animal Race [Brian Astle].ch8")
 
 method draw(self: Chip8Renderer) {.gdsync.} =
@@ -20,7 +20,9 @@ method draw(self: Chip8Renderer) {.gdsync.} =
   discard
 
 method process(self: Chip8Renderer, delta: float64) {.gdsync.} =
-  print("process")
+  # print("process")
+  # For now, let's update the drawing all the time
+  queue_redraw(self)
   discard
 
 method input(self: Chip8Renderer, event: InputEvent) {.gdsync.} =
